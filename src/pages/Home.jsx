386992b199428754560d73e3329a33c8489c901d
abulfeedy <66095707+abulfeedy@users.noreply.github.com>
2025-05-03 17:01:22 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -225,7 +225,7 @@ function Home() {
       {/* Features Section */}
       <section className='py-12'>
         <h2 className='text-3xl font-bold text-gray-800 mb-8 text-center'>
-          Why Choose PinkLaunch?
+          Why Choose Quba Launch?
         </h2>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
           {[
